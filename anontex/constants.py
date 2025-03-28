a27d216d0d5f7c43ec97ab0c5b56c1ac6d677e4e
ChamathKB <chamathkbatta@gmail.com>
2025-03-29 01:59:29 +0530

    feat: add cli and server routes

--- anontex/constants.py
+++ anontex/constants.py
@@ -3,4 +3,5 @@ import os
 TARGET = "https://api.openai.com"  # Target server
 ENTITY_TTL = 600  # 10 minutes
 ENTITY_LIST = ["PHONE_NUMBER", "PERSON", "EMAIL_ADDRESS", "LOCATION", "ORGANIZATION", "CREDIT_CARD"]
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+DEFAULT_CONFIG_PATH = os.getenv("DEFAULT_CONFIG_PATH", "anontex/languages-config.yml")
