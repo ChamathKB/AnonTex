04d45ac2040852ff70cc061812b30055de47c697
ChamathKB <chamathkbatta@gmail.com>
2025-04-09 12:11:01 +0530

    refactor: update constant file

--- anontex/constants.py
+++ anontex/constants.py
@@ -1,10 +1,10 @@
 import logging
 import os
 
-TARGET = "https://api.openai.com"  # Target server
-ENTITY_TTL = 600  # 10 minutes
-ENTITY_LIST = ["PHONE_NUMBER", "PERSON", "EMAIL_ADDRESS", "LOCATION", "ORGANIZATION", "CREDIT_CARD"]
+TARGET = os.getenv("TARGET", "https://api.openai.com")  # Target server
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+ENTITY_TTL = os.getenv("ENTITY_TTL", 600)  # 10 minutes
+ENTITY_LIST = ["PHONE_NUMBER", "PERSON", "EMAIL_ADDRESS", "LOCATION", "ORGANIZATION", "CREDIT_CARD"]
 DEFAULT_CONFIG_PATH = os.getenv("DEFAULT_CONFIG_PATH", "anontex/languages-config.yml")
 LOG_LEVELS = {
     "debug": logging.DEBUG,
